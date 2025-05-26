9b813f310c179bda72ef5797c6a84314603c7d51
khushiey <rajputkhushi170605@gmail.com>
2025-05-26 21:19:44 +0530

    khfj

---
+++ src/component/Home.jsx
@@ -0,0 +1,51 @@
+import '../style/Home.css'
+
+export default function Home() {
+    return (
+        <>
+            <div className="body">
+                <div className="container">
+                    <div className="bio">
+                        <div className="image">
+                            <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/A._P._J._Abdul_Kalam.jpg/500px-A._P._J._Abdul_Kalam.jpg' />
+                        </div>
+                        <div>
+                            <h1>A. P. J. Abdul Kalam </h1>
+                            <h4>1931-2015</h4>
+                        </div>
+
+
+                    </div>
+                    <div className="about">
+                        <h1>Missile Man of India</h1>
+                        <p><span>Avul Pakir Jainulabdeen Abdul Kalam</span> (/ˈɑːbdəl kəˈlɑːm/ (About this soundlisten); 15 October 1931 – 27 July 2015) was an Indian aerospace
+                            scientist who served as the 11th president of India from 2002 to 2007.
+                            He was born and raised in Rameswaram, Tamil Nadu and studied physics and aerospace engineering.
+                            He spent the next four decades as a scientist and science administrator,
+                            mainly at the Defence Research and Development Organisation (DRDO) and Indian Space Research Organisation (ISRO)
+                            and was intimately involved in India's civilian space programme and military missile development efforts.[1]
+                            He thus came to be known as the Missile Man of India for his work on the development of ballistic missile and launch vehicle technology.[2][3][4]
+                            He also played a pivotal organisational, technical,
+                            and political role in India's Pokhran-II nuclear tests in 1998, the first since the original nuclear test by India in 1974.</p>
+                    </div>
+                    <div className="biography">
+                        <h1>Biographies</h1>
+                        <p>
+
+                            Eternal Quest: Life and Times of Dr Kalam by S Chandra; Pentagon Publishers, 2002.
+                            President A P J Abdul Kalam by R K Pruthi; Anmol Publications, 2002.
+                            A P J Abdul Kalam: The Visionary of India by K Bhushan, G Katyal; A P H Pub Corp, 2002.
+                            A Little Dream (documentary film) by P. Dhanapal; Minveli Media Works Private Limited, 2008.
+                            The Kalam Effect: My Years with the President by P M Nair; Harper Collins, 2008.
+                            My Days With Mahatma Abdul Kalam by Fr A K George; Novel Corporation, 2009.
+                            A.P.J. Abdul Kalam: A Life by Arun Tiwari; Haper Collins, 2015.
+                            The People's President: Dr A P J Abdul Kalam by S M Khan; Bloomsbury Publishing, 2016.</p>
+                    </div>
+
+
+                </div>
+            </div>
+
+        </>
+    )
+}
